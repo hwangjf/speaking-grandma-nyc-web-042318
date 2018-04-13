7added15af74218eebd4eeafbd87b97ad280fe4d
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-13 20:54:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,3 @@
 def speak_to_grandma (phrase)
-  if phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  else phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-end
 
-speak_to_grandma("any")
+end
